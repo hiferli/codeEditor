95945f89a83262a63d450e89b03a34d9d7130cfb
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-03-19 00:03:29 +0530

    Backend Input Validation

--- Backend/Controllers/SubmissionControllers.js
+++ Backend/Controllers/SubmissionControllers.js
@@ -1,6 +1,26 @@
 export const uploadSubmission = async (request , response) => {
-    return response.json({
-        "Status": "Success",
-        "Data": request.body
-    })
+    try {
+        // Getting all data from frontend
+        const { problemId , username , language , stdin , code } = request.body;
+
+        // Checking for undefinity
+        if(username === undefined){
+            return response.status(400).json({
+                'error': 'Please enter a valid username'
+            })
+        } else if(code === undefined || language === undefined || problemId === undefined){
+            return response.status(404).json({
+                'error': "Not found"
+            })
+        } 
+
+        // Run the code and get the results
+        // Store the code with the other informatoin in the database
+        return response.json(request.body)
+    } catch (error) {
+        // Return the error
+        return response.status(500).json({
+            'error': error.message
+        })
+    }
 }
