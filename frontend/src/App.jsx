57da06497ddcdf76f14bd3a239106de5d6cb2e5d
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-03-21 02:48:59 +0530

    Loading Problems in Home Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios, { all } from 'axios'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [loading, setLoading] = useState(false);
+	const [allProblems, setAllProblems] = useState([]);
 
+	const fetchProblems = async () => {
+		setLoading(true);
+
+		try {
+			const response = await fetch("http://localhost:3000/problems", {
+				method: "GET",
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+
+			if (response.ok) {
+				const result = await response.json();
+				setAllProblems(result.result);
+				// console.log(allProblems)
+			}
+		} catch (error) {
+			console.log('Error while fetching problems from database: ' + error);
+			alert(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchProblems();
+	}, []);
+	
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline text-red-300">
-				Hello world!
-			</h1>
-
+			<h1>TUFCode</h1>
+			{
+				loading ? <h1>Loading</h1> : 
+				<div>
+					{
+						allProblems && allProblems?.map(problem => (
+							<div key={problem.problem_id}>{problem.problem_title}</div>
+						))
+					}
+				</div>
+			}
 		</>
 	)
 }
