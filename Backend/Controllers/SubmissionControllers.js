93b1ff1e6ea8c45b958ce8bbf4ef9be1c78f3c03
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-03-20 19:38:26 +0530

    Creating Submission Routes

--- Backend/Controllers/SubmissionControllers.js
+++ Backend/Controllers/SubmissionControllers.js
@@ -1,26 +1,114 @@
-export const uploadSubmission = async (request , response) => {
-    try {
-        // Getting all data from frontend
-        const { problemId , username , language , stdin , code } = request.body;
+import database from '../DatabaseConnection.js'
 
-        // Checking for undefinity
-        if(username === undefined){
-            return response.status(400).json({
-                'error': 'Please enter a valid username'
+export const getAllSubmissions = (request, response) => {
+    database.query(`
+        SELECT * FROM submission;
+    ` , (error, result, fields) => {
+        if (error) {
+            return response.status(502).json({
+                'error': error
+            });
+        } else if (result) {
+            return response.status(201).json({
+                'result': result
             })
-        } else if(code === undefined || language === undefined || problemId === undefined){
-            return response.status(404).json({
-                'error': "Not found"
-            })
-        } 
+        }
+    })
+}
 
-        // Run the code and get the results
-        // Store the code with the other informatoin in the database
-        return response.json(request.body)
-    } catch (error) {
-        // Return the error
-        return response.status(500).json({
-            'error': error.message
+// Schema
+/*
+    CREATE TABLE IF NOT EXISTS submission
+    (
+        submission_id INT AUTO_INCREMENT PRIMARY KEY,
+        problem_id INT,
+        username VARCHAR(50),
+        submission_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        stdin TEXT,
+        status ENUM('Accepted', 'Wrong', 'TLE', 'MLE'),
+        programming_language VARCHAR(50),
+        code TEXT,
+        FOREIGN KEY (problem_id) REFERENCES problem(problem_id)
+    );
+
+*/
+
+export const uploadSubmission = async (request, response) => {
+    const {
+        submission_id,
+        problem_id,
+        username,
+        stdin,
+        status,
+        programming_language,
+        code
+    } = request.body;
+
+    if (
+        submission_id === undefined ||
+        problem_id === undefined ||
+        username === undefined ||
+        status === undefined ||
+        programming_language === undefined ||
+        code === undefined || 
+        stdin === undefined
+    ) {
+        return response.status(400).json({
+            'error': "Please enter all the information about the submission, including title, tag, description"
         })
+    } else {
+        database.query(`
+            INSERT INTO submission
+            (
+                submission_id,
+                problem_id,
+                username,
+                stdin,
+                status,
+                programming_language,
+                code
+            )
+            VALUES
+            (
+                '${submission_id}',
+                '${problem_id}',
+                '${username}',
+                '${stdin}',
+                '${status}',
+                '${programming_language}',
+                '${code}'
+            );
+        `
+            , (error, result, fields) => {
+                if (error) {
+                    return response.status(502).json({
+                        'error': error
+                    });
+                } else if (result) {
+                    return response.status(201).json({
+                        'message': "Submission added successfully",
+                        'result': result,
+                        'body': request.body
+                    })
+                }
+            })
     }
+}
+
+export const getSubmissionsById = async (request, response) => {
+    const id = request.params.id;
+
+    database.query(`
+        SELECT * FROM submission WHERE submission_id = ${id};
+    ` , (error, result, fields) => {
+        if (error) {
+            return response.status(502).json({
+                'error': error
+            });
+        } else if (result) {
+            return response.status(201).json({
+                'result': result
+            })
+        }
+    })
 }
--- Backend/Routes/SubmissionRoutes.js
+++ Backend/Routes/SubmissionRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { uploadSubmission } from '../Controllers/SubmissionControllers.js';
+import { getAllSubmissions, getSubmissionsById, uploadSubmission } from '../Controllers/SubmissionControllers.js';
 
 const router = express.Router();
 
+router.get('/' , getAllSubmissions);
 router.post('/' , uploadSubmission);
+router.get('/:id' , getSubmissionsById);
 
 export default router
